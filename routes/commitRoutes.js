8fe8b7abf95890217c3e2122fbd3e528ace97081
Matt Quade <quadem09@gmail.com>
2018-01-03 01:27:33 +0000

    removed checkAuth

--- routes/commitRoutes.js
+++ routes/commitRoutes.js
@@ -2,33 +2,35 @@ var express = require('express');
 var commitRouter = express.Router();
 var models = require('../path/to/folder/models');
 
-commitRouter.get('/:id', shared.checkAuth,function(req, res) {
+commitRouter.get('/:id', function(req, res) {
     models.trade
-    .findOne({
-        where: { id: req.params.id },
-        include: [
-            {
-                model: models.pirate,
-                as: 'author'
-            },
-            {
-                model: models.interest,
-                as: "commit",
-                include: {
+        .findOne({
+            where: { id: req.params.id },
+            include: [
+                {
                     model: models.pirate,
-                    as: 'pirate'
-                }
+                    as: 'author'
+            },
+                {
+                    model: models.interest,
+                    as: "commit",
+                    include: {
+                        model: models.pirate,
+                        as: 'pirate'
+                    }
             }
         ]
-    })
-    .then(function(foundCommit) {
-        console.log(foundCommit);
-        res.render('commit', { trades: foundCommit,
-                    pirate: req.session.pirate });
-    })
-    .catch(function(err) {
-        res.status(500).send(err);
-    });
+        })
+        .then(function(foundCommit) {
+            console.log(foundCommit);
+            res.render('commit', {
+                trades: foundCommit,
+                pirate: req.session.pirate
+            });
+        })
+        .catch(function(err) {
+            res.status(500).send(err);
+        });
 });
 
 module.exports = commitRouter;
